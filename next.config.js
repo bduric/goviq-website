31ed210d7e1eb2fa7f0fdaf9a590731319630e1a
durichbojan <durich.bojan@gmail.com>
2025-09-12 16:59:44 -0400

    Configure for GitHub Pages with custom domain goviq.us

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/goviq-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/goviq-website/' : ''
+  }
 }
 
 module.exports = nextConfig
